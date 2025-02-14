095fbfc3d928ef2bbf2f7d2df1303d544eeada51
anickacodes <anickalewis@pursuit.org>
2025-02-14 01:39:24 -0500

    installations: vite, react, tailwind, postcss, autoprefixer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <h1>Vite + React</h1>
+      <div className="bg-blue-500 text-gold">Hello World</div>
+
+    </>
+  )
+}
+
+export default App
